99c5ef3b74bf188d3ba51d233c744ab6371f3b15
Jonas Abreu <jonas@vidageek.net>
2012-11-19 00:28:11 -0200

    added support for the 22 functions

--- src/main/scala/vidageek/scaly/Scaly.scala
+++ src/main/scala/vidageek/scaly/Scaly.scala
@@ -1,31 +1,3 @@
 package vidageek.scaly
 
-import br.com.caelum.vraptor.ComponentRegistry
-import br.com.caelum.vraptor.http.route.Router
-import br.com.caelum.vraptor.ioc.{ApplicationScoped, StereotypeHandler}
-
 trait Scaly extends FunctionsToRequestCode with Rendering with AutomaticInjection with RequestMethods
-
-@ApplicationScoped
-class ScalyStereotype(componentRegistry : ComponentRegistry, router : Router) extends StereotypeHandler {
-
-  override def stereotype = classOf[ScalyAware]
-  override def handle(clazz : Class[_]) = {
-    println("analyzing scaly class %s".format(clazz.getName))
-    val scaly = clazz.newInstance().asInstanceOf[Scaly]
-    scaly.pathToCode.foreach { entry =>
-      val httpMethod = entry._1
-      val path = entry._2
-      val clazz = entry._3.clazz
-      val method = apply(clazz)
-      println("registering route: %s => %s".format(path, clazz.getName))
-      componentRegistry.register(clazz, clazz)
-      val builder = router.builderFor(path)
-      builder.`with`(httpMethod)
-      builder.is(clazz, method)
-      router.add(new ScalyRoute(builder.build, path))
-    }
-  }
-
-  private def apply(clazz : Class[_]) = clazz.getDeclaredMethods().filter(_.getName == "apply").head
-}
--- src/main/scala/vidageek/scaly/VraptorToScaly.scala
+++ src/main/scala/vidageek/scaly/VraptorToScaly.scala
@@ -7,12 +7,38 @@ import br.com.caelum.vraptor.resource.ResourceMethod
 import java.lang.annotation.Annotation
 import br.com.caelum.vraptor.ioc.spring.SpringProvider
 import br.com.caelum.vraptor.ComponentRegistry
+import br.com.caelum.vraptor.ioc.ApplicationScoped
+import br.com.caelum.vraptor.http.route.Router
+import br.com.caelum.vraptor.ioc.StereotypeHandler
 
 class ScalyProvider extends SpringProvider {
 
   override def registerCustomComponents(registry : ComponentRegistry) = {
     registry.register(classOf[ScalyStereotype], classOf[ScalyStereotype]);
   }
+}
+
+@ApplicationScoped
+class ScalyStereotype(componentRegistry : ComponentRegistry, router : Router) extends StereotypeHandler {
+
+  override def stereotype = classOf[ScalyAware]
+  override def handle(clazz : Class[_]) = {
+    println("analyzing scaly class %s".format(clazz.getName))
+    val scaly = clazz.newInstance().asInstanceOf[Scaly]
+    scaly.pathToCode.foreach { entry =>
+      val (httpMethod, path, code) = entry
+      val clazz = code.clazz
+      val method = apply(clazz)
+      println("registering route: %s => %s".format(path, clazz.getName))
+      componentRegistry.register(clazz, clazz)
+      val builder = router.builderFor(path)
+      builder.`with`(httpMethod)
+      builder.is(clazz, method)
+      router.add(new ScalyRoute(builder.build, path))
+    }
+  }
+
+  private def apply(clazz : Class[_]) = clazz.getDeclaredMethods().filter(_.getName == "apply").head
 }
 
 class ScalyRoute(delegate : Route, path : String) extends Route {
